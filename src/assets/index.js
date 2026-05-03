7eb20e5f7db15ca9eb8dec7de532423ce4b177d9
Bahram2006 <yardam034@gmail.com>
2026-05-03 10:02:07 +0500

    index.js updated and problem sorver

--- src/assets/index.js
+++ src/assets/index.js
@@ -54,17 +54,6 @@ import sub4_4 from './4-4.png';
 import sub4_5 from './4-5.png';
 import sub5_1 from './5-1.png';
 import sub5_2 from './5-2.png';
-import sub5_3 from './5-3.png';
-import sub5_4 from './5-4.png';
-import sub5_5 from './5-5.png';
-import sub5_6 from './5-6.png';
-import sub5_7 from './5-7.png';
-import sub5_8 from './5-8.png';
-import sub5_9 from './5-9.png';
-import sub5_10 from './5-10.png';
-import sub5_11 from './5-11.png';
-import sub5_12 from './5-12.png';
-import sub5_13 from './5-13.png';
 import sub6_1 from './6-1.png';
 import sub6_2 from './6-2.png';
 import sub6_3 from './6-3.png';
@@ -79,49 +68,42 @@ import sub7_1 from './7-1.png';
 import sub7_2 from './7-2.png';
 import sub7_3 from './7-3.png';
 import sub7_4 from './7-4.png';
+import sub7_5 from './7-5.png';
 import sub7_6 from './7-6.png';
 import sub7_7 from './7-7.png';
 import sub7_8 from './7-8.png';
 import sub7_9 from './7-9.png';
 import sub7_10 from './7-10.png';
-import sub7_11 from './7-11.png';
 import sub8_1 from './8-1.png';
 import sub8_2 from './8-2.png';
 import sub8_3 from './8-3.png';
 import sub8_4 from './8-4.png';
 import sub8_5 from './8-5.png';
 import sub8_6 from './8-6.png';
+import sub8_7 from './8-7.png';
+import sub8_8 from './8-8.png';
+import sub8_9 from './8-9.png';
+import sub8_10 from './8-10.png';
 import sub9_1 from './9-1.png';
 import sub9_2 from './9-2.png';
+import sub9_3 from './9-3.png';
+import sub9_4 from './9-4.png';
+import sub9_5 from './9-5.png';
+import sub9_6 from './9-6.png';
 import sub10_1 from './10-1.png';
 import sub10_2 from './10-2.png';
-import sub10_3 from './10-3.png';
-import sub10_4 from './10-4.png';
-import sub10_5 from './10-5.png';
-import sub10_6 from './10-6.png';
 import sub11_1 from './11-1.png';
 import sub11_2 from './11-2.png';
 import sub11_3 from './11-3.png';
 import sub11_4 from './11-4.png';
+import sub11_5 from './11-5.png';
+import sub11_6 from './11-6.png';
 import sub12_1 from './12-1.png';
 import sub12_2 from './12-2.png';
+import sub12_3 from './12-3.png';
+import sub12_4 from './12-4.png';
 import sub13_1 from './13-1.png';
 import sub13_2 from './13-2.png';
-import sub13_3 from './13-3.png';
-import sub13_4 from './13-4.png';
-import sub13_5 from './13-5.png';
-import sub13_6 from './13-6.png';
-import sub13_7 from './13-7.png';
-import sub13_8 from './13-8.png';
-import sub13_9 from './13-9.png';
-import sub13_10 from './13-10.png';
-import sub13_11 from './13-11.png';
-import sub13_12 from './13-12.png';
-import sub13_13 from './13-13.png';
-import sub13_14 from './13-14.png';
-import sub13_15 from './13-15.png';
-import sub13_16 from './13-16.png';
-import sub13_17 from './13-17.png';
 import sub14_1 from './14-1.png';
 import sub14_2 from './14-2.png';
 import sub14_3 from './14-3.png';
@@ -131,6 +113,14 @@ import sub14_6 from './14-6.png';
 import sub14_7 from './14-7.png';
 import sub14_8 from './14-8.png';
 import sub14_9 from './14-9.png';
+import sub14_10 from './14-10.png';
+import sub14_11 from './14-11.png';
+import sub14_12 from './14-12.png';
+import sub14_13 from './14-13.png';
+import sub14_14 from './14-14.png';
+import sub14_15 from './14-15.png';
+import sub14_16 from './14-16.png';
+import sub14_17 from './14-17.png';
 import sub15_1 from './15-1.png';
 import sub15_2 from './15-2.png';
 import sub15_3 from './15-3.png';
@@ -140,8 +130,18 @@ import sub15_6 from './15-6.png';
 import sub15_7 from './15-7.png';
 import sub15_8 from './15-8.png';
 import sub15_9 from './15-9.png';
-import sub15_10 from './15-10.png';
-import sub15_11 from './15-11.png';
+import sub16_1 from './16-1.png';
+import sub16_2 from './16-2.png';
+import sub16_3 from './16-3.png';
+import sub16_4 from './16-4.png';
+import sub16_5 from './16-5.png';
+import sub16_6 from './16-6.png';
+import sub16_7 from './16-7.png';
+import sub16_8 from './16-8.png';
+import sub16_9 from './16-9.png';
+import sub16_10 from './16-10.png';
+import sub16_11 from './16-11.png';
+
 
 // Ählisini bir obýektiň içinde daşary eksport edýäris
 export const IMAGES = {
@@ -150,15 +150,16 @@ export const IMAGES = {
   sub2_1, sub2_2, sub2_3, sub2_4, sub2_5, sub2_6, sub2_7, sub2_8, sub2_9, sub2_10, sub2_11, sub2_12,
   sub3_1, sub3_2, sub3_3, sub3_4, sub3_5, sub3_6, sub3_7, sub3_8, sub3_9, sub3_10, sub3_11, sub3_12, sub3_13,
   sub4_1, sub4_2, sub4_3, sub4_4, sub4_5,
-  sub5_1, sub5_2, sub5_3, sub5_4, sub5_5, sub5_6, sub5_7, sub5_8, sub5_9, sub5_10, sub5_11, sub5_12, sub5_13,  
-  sub6_1, sub6_2, sub6_3, sub6_4, sub6_5, sub6_6, sub6_7, sub6_8, sub6_9, sub6_10,
-  sub7_1, sub7_2, sub7_3, sub7_4, sub7_6, sub7_7, sub7_8, sub7_9, sub7_10, sub7_11,
-  sub8_1, sub8_2, sub8_3, sub8_4, sub8_5, sub8_6,
-  sub9_1, sub9_2,
-  sub10_1, sub10_2, sub10_3, sub10_4, sub10_5, sub10_6,
-  sub11_1, sub11_2, sub11_3, sub11_4,
-  sub12_1, sub12_2,
-  sub13_1, sub13_2, sub13_3, sub13_4, sub13_5, sub13_6, sub13_7, sub13_8, sub13_9, sub13_10, sub13_11, sub13_12, sub13_13, sub13_14, sub13_15, sub13_16, sub13_17,
-  sub14_1, sub14_2, sub14_3, sub14_4, sub14_5, sub14_6, sub14_7, sub14_8, sub14_9,
-  sub15_1, sub15_2, sub15_3, sub15_4, sub15_5, sub15_6, sub15_7, sub15_8, sub15_9, sub15_10, sub15_11,
+  sub5_1, sub5_2,
+  sub6_1, sub6_2, sub6_3,sub6_4, sub6_5, sub6_6, sub6_7, sub6_8, sub6_9, sub6_10,
+  sub7_1, sub7_2, sub7_3, sub7_4, sub7_5, sub7_6, sub7_7, sub7_8, sub7_9, sub7_10,
+  sub8_1, sub8_2, sub8_3, sub8_4, sub8_5, sub8_6, sub8_7, sub8_8, sub8_9, sub8_10,
+  sub9_1, sub9_2, sub9_3, sub9_4, sub9_5, sub9_6,
+  sub10_1, sub10_2,
+  sub11_1, sub11_2, sub11_3, sub11_4, sub11_5, sub11_6,
+  sub12_1, sub12_2, sub12_3, sub12_4,
+  sub13_1, sub13_2,
+  sub14_1, sub14_2, sub14_3, sub14_4, sub14_5, sub14_6, sub14_7, sub14_8, sub14_9, sub14_10, sub14_11, sub14_12, sub14_13, sub14_14, sub14_15, sub14_16, sub14_17,
+  sub15_1, sub15_2, sub15_3, sub15_4, sub15_5, sub15_6, sub15_7, sub15_8, sub15_9,
+  sub16_1, sub16_2, sub16_3, sub16_4, sub16_5, sub16_6, sub16_7, sub16_8, sub16_9, sub16_10, sub16_11,
 };
